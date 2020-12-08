d6cc4a224cb67075687a9d2404e630c8f402eb59
Jared Potter <jaredpotter1@gmail.com>
2020-12-07 21:22:04 -0700

    let's try this

--- functions/index.js
+++ functions/index.js
@@ -11,10 +11,15 @@ admin.initializeApp({
   // databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
 });
 
+const firestore = admin.firestore();
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true });
+
+  firestore.collection('things').add({ name: 'a thing' });
+
   response.send('Hello from Firebase!');
 });
