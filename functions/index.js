c36e01613b1854f9767a8ce83c4e80251f71c1b4
Jared Potter <jaredpotter1@gmail.com>
2020-12-07 22:26:37 -0700

    final

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-const FIREBASE_TOKEN = process.env.FIREBASE_TOKEN;
-
-console.log('FIREBASE_TOKEN: ' + FIREBASE_TOKEN);
-
 admin.initializeApp({
-  // credential: admin.credential.refreshToken(FIREBASE_TOKEN),
   credential: admin.credential.applicationDefault(),
-  // databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
 });
 
 const firestore = admin.firestore();
